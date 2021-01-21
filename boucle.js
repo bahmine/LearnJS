c96fba58d1265c2185db6410736e57c1c39f2855
BAH Mamadou Lamine <bahmine96@yahoo.fr>
2021-01-21 11:33:16 +0100

    ajout

---
+++ boucle.js
@@ -0,0 +1,10 @@
+let Passengers = [
+    "Lamine","Imane","Saliou","Samba","Bhoye"
+];
+console.log ('------for.......in');
+for (let i in Passengers) 
+    console.log (Passengers[i]);
+
+console.log ('------for.......of');
+for (let p of Passengers)
+    console.log (p);
--- indexTab.js
+++ indexTab.js
@@ -1,56 +1,38 @@
 class Episode {
-  constructor(title, duration, minutesWatched) {
+  constructor(title, duration, hasBeenWatched) {
     this.title = title;
     this.duration = duration;
-    // Add logic here
-    // ======================
-    /*if (minutesWatched === duration) {
-      this.watchedText = "Déjà regardé";
-      this.continueWatching = false;
-    } else if (minutesWatched > 0 && minutesWatched < duration) {
-      this.watchedText ="En train regardé";
-      this.continueWatching = true;
-    } else {
-      this.watchedText = "Pas encore regardé";
-      this.continueWatching = false;
-    }*/
-    if (minutesWatched === duration || minutesWatched === 0) {
-      this.watchedText = minutesWatched === 0? "Pas encore regardé": "Déjà regardé";
-      this.continueWatching = false;
-    } else {
-      this.watchedText = "En train de regardé";
-      this.continueWatching = true;
-    }
-    
-    
-    // ======================
+    this.hasBeenWatched = hasBeenWatched;
   }
 }
 
-let firstEpisode = new Episode('Dark Beginnings', 45, 45);
-let secondEpisode = new Episode('The Mystery Continues', 45, 10);
-let thirdEpisode = new Episode('An Unexpected Climax', 60, 0);
+let episodes = [
+  new Episode('Dark Beginnings', 45, true),
+  new Episode('The Mystery Continues', 45, false),
+  new Episode('An Unexpected Climax', 60, false)
+];
 
-let episodes = [firstEpisode, secondEpisode, thirdEpisode];
+// Add logic here
+// ======================
+
+for (let i in episodes) {
+  episodes[i].hasBeenWatched = false;
+}
+    
+// ======================
 
 const body = document.querySelector('body');
 
-for(let i = 0; i < episodes.length; i++) {
+for(let episode of episodes) {
   let newDiv = document.createElement('div');
   newDiv.classList.add('series-frame');
   let newTitle = document.createElement('h2');
   newTitle.innerText = 'The Story of Tau';
   let newParagraph = document.createElement('p');
-  newParagraph.innerText = `${episodes[i].title}
-  ${episodes[i].duration} minutes
-  ${episodes[i].watchedText}
-  `;
-    newDiv.append(newTitle);
-    newDiv.append(newParagraph);
-    if (episodes[i].continueWatching) {
-      let newButton = document.createElement('button');
-      newButton.innerText = 'Continue watching';
-      newParagraph.append(newButton);
-    }
-    body.append(newDiv);
+  newParagraph.innerText = `${episode.title}
+  ${episode.duration} minutes
+  ${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+  newDiv.append(newTitle);
+  newDiv.append(newParagraph);
+  body.append(newDiv);
 }
